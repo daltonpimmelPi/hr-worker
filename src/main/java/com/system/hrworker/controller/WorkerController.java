ea2b7cd3d908b2380a43be165a25b3cb283df8cc
Dalton <dalton@viasoft.com.br>
2021-01-18 17:36:44 -0300

    hr-worker implementation

--- src/main/java/com/system/hrworker/controller/WorkerController.java
+++ src/main/java/com/system/hrworker/controller/WorkerController.java
@@ -2,7 +2,10 @@ package com.system.hrworker.controller;
 
 import com.system.hrworker.entities.Worker;
 import com.system.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +21,11 @@ public class WorkerController {
     @Autowired
     private WorkerRepository workerRepository;
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Autowired
+    private Environment env; //informações do contexto da aplicação
+
     @GetMapping
     public ResponseEntity<List<Worker>> findAll(){
         List<Worker> workers = workerRepository.findAll();
@@ -26,6 +34,9 @@ public class WorkerController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
+
+        logger.info("PORT = " + env.getProperty("local.server.port")); //numero da porta que esta rodando
+
         Worker workers = workerRepository.findById(id).get();
         return ResponseEntity.ok(workers);
     }
